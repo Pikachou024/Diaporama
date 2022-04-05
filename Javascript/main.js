ca5e4c2af30bd025415059ff02cc406f407b1d57
Pikachou024 <101394301+Pikachou024@users.noreply.github.com>
2022-04-05 15:16:20 +0200

    Add files via upload

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,80 +1,89 @@
-let tableau;
-let i;
-let arret;
+// let largeur = innerWidth;
+// if (innerWidth <= 705) {
+//     console.log(innerWidth);
+// document.querySelector(".SectionFleche-gauche").classList.toggle("order1")
+// }
+// else {
+//     document.querySelector(".SectionFleche-gauche").classList.toggle("order1")
+// }
 
+let mainindex
+mainindex = 0
+let tableauPictures = document.querySelectorAll(".ListImages img")
+
+let arret;
+let image;
+image = document.querySelector(".image01");
+image.classList.add("affiche");
+
+function afficheImage() {
+    for (let j = 0; j < tableauPictures.length; j++) {
+        tableauPictures[j].classList.remove("affiche")
+    }
+    tableauPictures[mainindex].classList.add("affiche");
+}
 
 let FlecheDroite;
 FlecheDroite = document.querySelector(".SectionFleche-droite");
 FlecheDroite.addEventListener("click", AfficheCacheD)
 
-let image;
-image = document.querySelector(".image01");
-image.classList.add("affiche");
-
-
-i = 0
-
-function AfficheCacheD(tableau) {
-
-    tableau = [".image01", ".image02", ".image03", ".image04", ".image05", ".image06", ".image07"];
-    document.querySelector(tableau[i]).classList.remove("affiche");
-    i++
-    document.querySelector(tableau[i]).classList.add("affiche");
-
-    if (i == 6) {
+function AfficheCacheD() {
+    mainindex++
+    afficheImage()
+    console.log(mainindex);
+    if (mainindex == 6) {
         FlecheDroite.removeEventListener("click", AfficheCacheD)
         clearInterval(arret)
     }
     FlecheGauche.addEventListener("click", AfficheCacheG);
-
 }
-
-
 
 let FlecheGauche;
 FlecheGauche = document.querySelector(".SectionFleche-gauche");
 FlecheGauche.addEventListener("click", AfficheCacheG);
-function AfficheCacheG(tableau) {
-    tableau = [".image01", ".image02", ".image03", ".image04", ".image05", ".image06", ".image07"];
-    document.querySelector(tableau[i]).classList.remove("affiche");
-    document.querySelector(tableau[i - 1]).classList.add("affiche");
-    i--
-    if (i == 0) {
-        FlecheGauche.removeEventListener("click", AfficheCacheG)
 
+function AfficheCacheG() {
+    mainindex--
+    afficheImage()
+    if (mainindex == 0) {
+        FlecheGauche.removeEventListener("click", AfficheCacheG)
     }
     FlecheDroite.addEventListener("click", AfficheCacheD)
-}
 
+}
 
 let sectionPlay;
 sectionPlay = document.querySelector(".SectionButton-play");
 sectionPlay.addEventListener("click", playDiapo);
 
 function playDiapo() {
-    arret = setInterval(diapoImage, 1000);
-
+    arret = setInterval(diapoImage, 2000);
     function diapoImage() {
-        tableau = [".image01", ".image02", ".image03", ".image04", ".image05", ".image06", ".image07"];
-        AfficheCacheD(tableau)
-        console.log("hola");
+        AfficheCacheD()
     }
+
     let sectionPause;
     sectionPause = document.querySelector(".SectionButton-pause");
     sectionPause.addEventListener("click", function () {
         clearInterval(arret)
     });
 }
-let allbuton;
-allbuton = document.querySelectorAll(".HeaderPuce div");
-console.log(allbuton);
-// for (let index = 0, index < ; index++ )
-allbuton.addEventListener("click", navButton);
 
-function navButton() {
-    console.log("coucou");
+let tableauPuces = document.querySelectorAll(".HeaderPuce div")
+for (let i = 0; i < tableauPuces.length; i++) {
+    tableauPuces[i].addEventListener("click", clickPuce);
 }
 
+function clickPuce(event) {
+    for (let j = 0; j < tableauPictures.length; j++) {
+        tableauPictures[j].classList.remove("affiche")
+    }
+    let onClickPuce = event.currentTarget;
+    let newindex = onClickPuce.dataset.index;
+    mainindex = parseInt(newindex)
+    afficheImage()
+    FlecheGauche.addEventListener("click", AfficheCacheG);
+    FlecheDroite.addEventListener("click", AfficheCacheD)
 
 
-
+}
